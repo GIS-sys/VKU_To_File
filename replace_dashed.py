ea7203bfaf377dcd4e690670ea12fa18d75189f7
EgorovGA <egorov.ga@minc.tech>
2024-03-25 14:29:47 +0300

    dash replaces attempt

--- replace_dashed.py
+++ replace_dashed.py
@@ -1,8 +1,20 @@
-filename = input("File: ")
-replaces = {"A2", "A3"}
+def do_replaces(files, replaces):
+    for filename in files:
+        with open(filename, "r") as f:
+            text = f.read()
+        for replace_what, replace_how in replaces.items():
+            text = text.replace(replace_what, replace_how)
+        with open(filename, "w") as f:
+            f.write(text)
 
-with open(filename, "w") as f:
-    text = f.read()
-    for replace_what, replace_how in replaces:
-        text.replace(replace_what, replace_how)
-    f.write(text)
+
+# errors
+errors = input("Play errors with dashes")
+errors = [e["entityId"] for e in json.loads(errors)["versionDataErrors"]]
+replaces = {e: e.replace("-", "_") for e in errors if "-" in e}
+
+# files
+files = [f.strip() for f in input("Files: ").split(",")]
+
+# do
+do_replaces(files, replaces)
